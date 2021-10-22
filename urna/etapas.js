2dd6e836f8b2e2e56e96d71985dc307d3df8245e
Lucca Rendall <luccarendall2@gmail.com>
2021-10-22 16:38:13 -0300

    urna quase pronta

--- urna/etapas.js
+++ urna/etapas.js
@@ -1,50 +1,64 @@
 let etapas = [
     {
-titulo: 'VEREADOR',
-numeros: 5,
-candidatos: [
-    {
-        numero: 21011,
-        nome: 'Samwise Gamgee',
-        partido: 'PTS',
-        fotos:[
-        {url:'21011.jpg', legenda: 'Vereador'}
+      //Vereadores
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [{
+                numero: '21011',
+                nome: 'Samwise Gamgee',
+                partido: 'PTS',
+                fotos: [{
+                    url: '21011.jpg',
+                    legenda: 'Vereador'
+                }]
+            },
+            {
+                numero: '34004',
+                nome: 'Frodo Baggins',
+                partido: 'PSG',
+                fotos: [{
+                    url: '34004.jpg',
+                    legenda: 'Vereador'
+                }]
+            },
         ]
     },
     {
-    numero: 34004,
-    nome: 'Frodo Baggins',
-    partido: 'PSG',
-    fotos:[
-        {url:'34004.jpg', legenda: 'Vereador'}
-    ]
-},
-]
-},
-{
+        //Prefeitos
         titulo: 'PREFEITO',
         numeros: 2,
-        candidatos:[
-    {
-        numero: 11,
-        nome: 'Gandalf, The Gray',
-        partido: 'PTM',
-        vice: 'Bilbo Baggins',
-        fotos: [
-            {url:'11.jpg', legenda: 'Prefeito'},
-            {url:'11_1.jpg', legenda: 'Vice-Prefeito', small: true}
-            ]
-    },
-    {
-        numero: 15,
-        nome: 'Sauron',
-        partido: 'PDO',
-        vice: 'Azog, The Defiler',
-        fotos:[
-        {url:'15.jpg', legenda: 'Prefeito'},
-        {url:'15_1.jpg', legenda: 'Vice-Prefeito', small: true}
+        candidatos: [{
+                numero: '11',
+                nome: 'Gandalf, The Gray',
+                partido: 'PTM',
+                vice: 'Bilbo Baggins',
+                fotos: [{
+                        url: '11.jpg',
+                        legenda: 'Prefeito'
+                    },
+                    {
+                        url: '11_1.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            },
+            {
+                numero: '15',
+                nome: 'Sauron',
+                partido: 'PDO',
+                vice: 'Azog, The Defiler',
+                fotos: [{
+                        url: '15.jpg',
+                        legenda: 'Prefeito'
+                    },
+                    {
+                        url: '15_1.jpg',
+                        legenda: 'Vice-Prefeito',
+                        small: true
+                    }
+                ]
+            },
         ]
-},
-]
-}
+    }
 ];
--- urna/script.js
+++ urna/script.js
@@ -1,21 +1,29 @@
 let seuVotoPara = document.querySelector('.divisao-1-1 span');
 let cargo = document.querySelector('.divisao-1-2 span');
-let descricao = document.querySelector('.divisao-1-4 span');
-let aviso = document.querySelector('.divisao-2 span');
-let lateral = document.querySelector('.divisao-1-right span');
-let numeros = document.querySelector('.divisao-1-3 span');
+let descricao = document.querySelector('.divisao-1-4');
+let aviso = document.querySelector('.divisao-2');
+let lateral = document.querySelector('.divisao-1-right');
+let numeros = document.querySelector('.divisao-1-3');
 
 let etapaAtual = 0;
 let numero = '';
-
+let votoBranco = false;
+let votos = [];
 
 function comecarEtapa() {
     let etapa = etapas[etapaAtual];
-    
+
     let numeroHtml = '';
-    
-    for(let i=0;i<etapa.numeros;i++) {
-        numeroHtml += '<div class="numero"></div>';
+    numero = '';
+    votoBranco = false;
+
+
+    for (let i = 0; i < etapa.numeros; i++) {
+        if (i === 0) {
+            numeroHtml += 'div class= "numero pisca"><div>';
+        } else {
+            numeroHtml += '<div class="numero"></div>';
+        }
     }
 
     seuVotoPara.style.display = 'none';
@@ -25,22 +33,103 @@ function comecarEtapa() {
     lateral.innerHTML = '';
     numeros.innerHTML = numeroHtml;
 }
-function atualizaInterface() {
-    
-}
 
+function atualizaInterface() {
+    let etapa = etapas[etapaAtual];
+    let candidato = etapa.candidatos.filter((item) => {
+        if (item.numero === numero) {
+            return true;
+        } else {
+            return false;
+        }
+    });
+    if (candidato.length > 0) {
+        candidato = candidato[0];
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = `
+        Nome: ${candidato.nome}</br> 
+        Partido: ${candidato.partido}</br>`;
+
+        let fotosHtml = '';
+        for (let i in candidato.fotos) {
+            if (candidato.fotos[i].small === true) {
+                fotosHtml += `<div class="divisao-1-image small"><img
+            src="img/${candidato.fotos[i].url}">${candidato.fotos[i].legenda}</div>`
+            } else {
+                fotosHtml += `<div class="divisao-1-image"><img src="img/${candidato.fotos[i].url}">${candidato.fotos[i].legenda}</div>`
+
+            }
+        }
+        lateral.innerHTML = fotosHtml;
+
+    } else {
+
+        seuVotoPara.style.display = 'block';
+        aviso.style.display = 'block';
+        descricao.innerHTML = '<div class="aviso--grande
+        pisca ">VOTO NULO</div>'
+    }
+}
 
 function clicou(n) {
-    alert("Clicou em "+n);
+    let elNumero = document.querySelector('.numero.pisca');
+    if (elNumero !== null) {
+        elNumero.innerHTML = n;
+        numero = `${numero}${n}`;
+
+        elNumero.classList.remove('pisca')
+        if (elNumero.nextElementSibling !== null) {
+            elNumero.nextElementSibling.classList.add('pisca')
+        } else {
+            atualizaInterface();
+        }
+    }
 }
+
 function branco() {
-    alert("Clicou em BRANCO");
+    if (numero === '') {
+        votoBranco = true;
+        seuVotoPara.style.display = 'block';
+        lateral.innerHTML = '';
+        aviso.style.display = 'block';
+        numeros.innerHTML = '';
+        descricao.innerHTML = '<div class="aviso--grande pisca">VOTO EM BRANCO</div>'
+    } else {
+        alert('Para votar em BRANCO, não pode ter digitado nenhum número!')
+    }
 }
+
 function corrige() {
-    alert("Clicou em CORRIGE");
+    comecarEtapa();
 }
+
 function confirma() {
-    alert("Clicou em CONFIRMA");
+    let etapa = etapas[etapaAtual];
+    let votoConfirmado = false;
+    if (votoBranco === true) {
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: 'branco'
+        })
+    } else if (numero.length === etapa.numeros) {
+        votoConfirmado = true;
+        votos.push({
+            etapa: etapas[etapaAtual].titulo,
+            voto: numero
+        })
+    }
+
+    if (votoConfirmado) {
+        etapaAtual++;
+        if (etapas[etapaAtual] !== undefined) {
+            comecarEtapa();
+        } else {
+            document.querySelector('.tela').innerHTML = '<div class="aviso--gigante pisca">FIM</div>'
+            console.log(votos)
+        }
+    }
 }
 
 comecarEtapa();
