4f19ce5c3d968783e6eb44e6b918cfba9ed534f8
Lucca Rendall <92706411+LuccaRendall@users.noreply.github.com>
2021-11-02 19:10:37 -0300

    Add files via upload

---
+++ etapas.js
@@ -0,0 +1,55 @@
+let etapas = [
+    {
+        // VEREADORES
+        titulo: 'VEREADOR',
+        numeros: 5,
+        candidatos: [
+            {
+                // CANDIDATO 1 - VEREADOR
+                numero: '12345',
+                nome: 'Samwise Gamgee',
+                partido: 'PTS',
+                fotos: [
+                    {url: '5.jpg', legenda: 'Vereador', small: false}
+                ]
+            },
+            {
+                // CANDIDATO 2 - VEREADOR
+                numero: '67890',
+                nome: 'Frodo Baggins',
+                partido: 'PSG',
+                fotos: [
+                    {url: '6.jpg', legenda: 'Vereador'}
+                ]
+            },
+
+        ]
+    },
+    {
+        // PREFEITOS
+        titulo: 'PREFEITO',
+        numeros: 2,
+        candidatos: [
+            {
+                numero: '11',
+                nome: 'Gandalf, The Gray',
+                partido: 'PdTM',
+                vice: 'Bilbo Baggins',
+                fotos: [
+                    {url: '1.jpg', legenda: 'Prefeito'},
+                    {url: '2.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+            {
+                numero: '12',
+                nome: 'Sauron',
+                partido: 'PDO',
+                vice: 'Saruman',
+                fotos: [
+                    {url: '3.jpg', legenda: 'Prefeito'},
+                    {url: '4.jpg', legenda: 'Vice-Prefeito', small: true}
+                ]
+            },
+        ]
+    }
+];
